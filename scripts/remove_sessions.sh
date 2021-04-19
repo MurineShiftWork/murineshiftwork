611e4e0ca0741efde92caa42ba803bc2a7fc9a79
Lars B. Rollik <L.B.Rollik@pm.me>
2021-04-19 14:32:37 +0100

    remove sessions script to use after uploading data

---
+++ scripts/remove_sessions.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+rm -rf /home/$USER/code/pybpod_lbr/main_project/experiments/MAIN_experiment/setups/MAIN_setup/sessions/*
+rm -rf /home/$USER/code/pybpod_lbr/main_project/experiments/TEST_experiment/setups/TEST_setup/sessions/*
