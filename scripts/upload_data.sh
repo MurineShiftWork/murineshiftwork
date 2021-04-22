4fe98964f604419c7e3e5a4f69f08beb9660eddd
Lars B. Rollik <L.B.Rollik@pm.me>
2021-04-22 18:13:38 +0100

    script with bash would execute as root

--- scripts/upload_data.sh
+++ scripts/upload_data.sh
@@ -4,14 +4,15 @@
 # 2) mkdir /home/$USER/credentials
 # 3) nano /home/$USER/credentials/$USER
 
-credentials_file="/home/$USER/credentials/$USER"
+username="lbr"
+credentials_file="/home/$username/credentials/$username"
 upload_dir="/mnt/winstor/sjones/users/lars/ACQUISITION/setups_pybpod/"
 mount_point="/mnt/winstor"
 
 sudo mount -t cifs -o credentials=$credentials_file,nodfs,uid=$(id -u),gid=$(id -g),_netdev,x-systemd.device-timeout=4,nofail //winstor.id.swc.ucl.ac.uk/winstor/swc $mount_point
 ls -lah $upload_dir
 
-rsync -a --info=progress2 "/home/$USER/code/pybpod_lbr/main_project" $upload_dir
+rsync -a --info=progress2 "/home/$username/code/pybpod_lbr/main_project" $upload_dir
 
 sudo umount -f $mount_point
 ls -lah $mount_point
