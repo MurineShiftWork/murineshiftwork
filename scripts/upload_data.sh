7ef6d14b0619d1f5fdbe2c5a3e7b1348c5e058e2
Lars B. Rollik <L.B.Rollik@pm.me>
2021-07-21 10:28:12 +0100

    remove old "main_project" from upload

--- scripts/upload_data.sh
+++ scripts/upload_data.sh
@@ -4,7 +4,8 @@
 find ~/data/ -type f -name "*.pyc" -delete
 find ~/data/ -type d -name "__pycache__" -exec rm -rfv {} +
 
-# remove test_subject from upload
-# remove unnamed acquisitions from video source
-# remvoe test protocols like _test__flush_water
-rsync -a --info=progress2 ~/data/* larsr@ssh.swc.ucl.ac.uk:/nfs/winstor/sjones/users/lars/ACQUISITION/setup_npx/ --exclude="_test_subject" --exclude="default_acq_name*" --exclude="*___test__*"
+# exclude test_subject from upload
+# exclude unnamed acquisitions from video source
+# exclude test protocols like _test__flush_water
+# exclude old "main_project" folder
+rsync -a --info=progress2 ~/data/* larsr@ssh.swc.ucl.ac.uk:/nfs/winstor/sjones/users/lars/ACQUISITION/setup_npx/ --exclude="_test_subject" --exclude="default_acq_name*" --exclude="*___test__*" --exclude="main_project"
