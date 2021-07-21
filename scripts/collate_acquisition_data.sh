2a8d3fae2fb675ab69391dedc7219dca29d0a4cd
Lars B. Rollik <L.B.Rollik@pm.me>
2021-07-21 10:17:19 +0100

    fix for collate script

--- scripts/collate_acquisition_data.sh
+++ scripts/collate_acquisition_data.sh
@@ -4,14 +4,18 @@ source_dir="/home/lbr/data/behaviour"
 target_dir="/home/lbr/data"
 # project_dir="murine_shift_work"
 
+
+# exclude protocols that contain test like _test__flush_water
+# exclude test_subject sessions without removing them locally
+
 for setup in setup1router setup2 setup3 setup4 setup5
 do
 	echo "\n - Loading from $setup : $source_dir -> $target_dir"
 	rsync -a --info=progress2 \
 		"$setup:$source_dir" \
 		$target_dir \
-		--exclude="_test_subject*" \	# exclude test_subject sessions without removing them locally
-		--exclude="*_test__*"		# exclude protocols that contain test like _test__flush_water
+		--exclude="_test_subject*" \
+		--exclude="*_test__*"
 done
 
 find $source_dir -type f -name "*.pyc" -delete
