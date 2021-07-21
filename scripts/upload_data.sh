f4b2a32c17e2eda38544c2034564015a6c8ee030
Lars B. Rollik <L.B.Rollik@pm.me>
2021-07-21 10:23:58 +0100

    newlines fixed in upload script

--- scripts/upload_data.sh
+++ scripts/upload_data.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 
-
+# remove unnecessary files .pyc and folders pycache
 find ~/data/ -type f -name "*.pyc" -delete
 find ~/data/ -type d -name "__pycache__" -exec rm -rfv {} +
 
-rsync -a \
-	--info=progress2 \
-	~/data/* \
-	larsr@ssh.swc.ucl.ac.uk:/nfs/winstor/sjones/users/lars/ACQUISITION/setup_npx/ \
-	--exclude="_test_subject" \	# remove test_subject from upload
-	--exclude="default_acq_name*" \	# remove unnamed acquisitions from video source
-	--exclude="*_test__*"		# remvoe test protocols like _test__flush_water
+# remove test_subject from upload
+# remove unnamed acquisitions from video source
+# remvoe test protocols like _test__flush_water
+rsync -a --info=progress2 ~/data/* larsr@ssh.swc.ucl.ac.uk:/nfs/winstor/sjones/users/lars/ACQUISITION/setup_npx/ --exclude="_test_subject" --exclude="default_acq_name*" --exclude="*_test__*"
