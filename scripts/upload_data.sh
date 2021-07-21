63de4940144a08155186d51a7a22daa16b9a9b17
Lars B. Rollik <L.B.Rollik@pm.me>
2021-07-21 09:56:38 +0100

    exclude test sessions from upload, but keep locally in case relevant. remove pycache and pyc before upload to shield remote from unnecessary files.

--- scripts/upload_data.sh
+++ scripts/upload_data.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
-rsync -a --info=progress2 ~/data/behaviour larsr@ssh.swc.ucl.ac.uk:/nfs/winstor/sjones/users/lars/ACQUISITION/setup_npx/
+
+find ~/data/behaviour -type f -name "*.pyc" -delete
+find ~/data/behaviour -type d -name "__pycache__" -exec rm -rfv {} +
+
+rsync -a \
+	--info=progress2 \
+	~/data/behaviour \
+	larsr@ssh.swc.ucl.ac.uk:/nfs/winstor/sjones/users/lars/ACQUISITION/setup_npx/ \
+	--exclude="_test_subject"
