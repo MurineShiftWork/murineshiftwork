75e83a9897aacbada0c0eb203d180c2b174ba222
Lars B. Rollik <L.B.Rollik@pm.me>
2021-08-07 11:29:42 +0100

    collate data and clean files for port4 events

--- scripts/collate_acquisition_data.sh
+++ scripts/collate_acquisition_data.sh
@@ -1,13 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
-source_dir="/home/lbr/data/behaviour"
-target_dir="/home/lbr/data"
-# project_dir="murine_shift_work"
+eval source_dir="~/data/behaviour"
+eval target_dir="~/data"
 
-
-# exclude protocols that contain test like _test__flush_water
-# exclude test_subject sessions without removing them locally
-
+# Collate data from all setups
 for setup in setup1router setup2 setup3 setup4 setup5
 do
 	echo "\n - Loading from $setup : $source_dir -> $target_dir"
@@ -15,15 +11,40 @@ do
 		"$setup:$source_dir" \
 		$target_dir \
 		--exclude="_test_subject*" \
-		--exclude="*_test__*"
+		--exclude="*_test__*" \
+		--exclude="*.pyc" \
+		--exclude="*__pycache__*"
+
 done
 
-find $source_dir -type f -name "*.pyc" -delete
-find $target_dir -type d -name "__pycache__" -exec rm -rfv {} +
+# echo "Removing remote files.."
+# Do NOT remove files from setup 1 - these will be uploaded!
+ssh setup2 rm -rf "${source_dir}/*/*"
+ssh setup3 rm -rf "${source_dir}/*/*"
+ssh setup4 rm -rf "${source_dir}/*/*"
+ssh setup5 rm -rf "${source_dir}/*/*"
 
-#rsync -a --info=progress2 "setup1router:$data_dir" $data_dir
-#rsync -a --info=progress2 "setup2:$data_dir" $data_dir
-#rsync -a --info=progress2 "setup3:$data_dir" $data_dir
-#rsync -a --info=progress2 "setup4:$data_dir" $data_dir
-#rsync -a --info=progress2 "setup5:$data_dir" $data_dir
-#rsync -a --info=progress2 "npxb:$data_dir" $data_dir
+# find $target_dir -type f -name "*.pyc" -delete
+# find $target_dir -type d -name "__pycache__" -exec rm -rfv {} +
+
+echo "Removing noise events 'Port4'..."
+ts=$(date +%Y%m%d_%H%M%S)
+
+find $target_dir -name "*.csv" -print0 | while read -d $'\0' file
+do
+	echo $file
+
+	if stat -t "${file}."* >/dev/null 2>&1
+	then
+		echo "" # Already exists. Do nothing.
+	else
+		echo "Cleaning file: ${file}"
+		sed "-i.bak.${ts}" '/Port4/d' $file
+	fi
+
+done
+
+# Alternative for cleaning files without checking if file exists:
+# find $target_dir -type f -name "*.csv" -exec sed "-i.bak.${ts}" '/Port4/d' {} +
+
+#
--- scripts/upload_data.sh
+++ scripts/upload_data.sh
@@ -8,4 +8,4 @@ find ~/data/ -type d -name "__pycache__" -exec rm -rfv {} +
 # exclude unnamed acquisitions from video source
 # exclude test protocols like _test__flush_water
 # exclude old "main_project" folder
-rsync -a --info=progress2 ~/data/* larsr@ssh.swc.ucl.ac.uk:/nfs/winstor/sjones/users/lars/ACQUISITION/setup_npx/ --exclude="_test_subject" --exclude="default_acq_name*" --exclude="*___test__*" --exclude="main_project"
+rsync -av --info=progress2 ~/data/* larsr@ssh.swc.ucl.ac.uk:/nfs/winstor/sjones/users/lars/ACQUISITION/setup_npx/ --exclude="_test_subject" --exclude="default_acq_name*" --exclude="*___test__*" --exclude="main_project"
