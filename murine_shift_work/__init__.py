233a00c935b30dd38bbd5bca21c089ae67cf6573
Lars B. Rollik <L.B.Rollik@pm.me>
2021-08-18 12:27:56 +0100

    logging fixes. fix for acquisition_group

--- murine_shift_work/__init__.py
+++ murine_shift_work/__init__.py
@@ -7,31 +7,32 @@ from murine_shift_work.logic.pybpod_helpers import patch_logging_levels
 from murine_shift_work.logic.pybpod_helpers import patch_user_settings
 from murine_shift_work.logic.task_process import run_msw_cli
 
+# Metadata
+__version__ = "0.0.2.dev0"
+__author__ = "Lars B. Rollik"
 
 # General logger
 level = "DEBUG"
 logger = logging.getLogger()
-logger.setLevel(getattr(logging, level))
 
-formatter = logging.Formatter(
-    "%(asctime)s.%(msecs)03d - %(levelname)s"
-    " - %(processName)s %(filename)s:%(lineno)s"
-    " - %(message)s"
-)
-formatter.datefmt = "%Y-%m-%d %H:%M:%S"
+if not logger.handlers:
+    logger.setLevel(getattr(logging, level))
 
-stream_handler = logging.StreamHandler()
-stream_handler.setLevel(getattr(logging, level))
-stream_handler.setFormatter(formatter)
-logger.addHandler(stream_handler)
+    formatter = logging.Formatter(
+        "%(asctime)s.%(msecs)03d - %(levelname)s"
+        " - %(processName)s %(filename)s:%(lineno)s"
+        " - %(message)s"
+    )
+    formatter.datefmt = "%Y-%m-%d %H:%M:%S"
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(getattr(logging, level))
+    stream_handler.setFormatter(formatter)
+    logger.addHandler(stream_handler)
 
 # Prepare pybpod package
 patch_user_settings()
 patch_logging_levels()
-
-# Metadata
-__version__ = "0.0.2.dev0"
-__author__ = "Lars B. Rollik"
 
 
 # Entrypoints
