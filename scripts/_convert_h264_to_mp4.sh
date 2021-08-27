6bc27fdaf8876e37642f6a4beb69f20a470e3a7b
Lars B. Rollik <L.B.Rollik@pm.me>
2021-08-27 15:04:30 +0100

    upload fixed. subject names regularised

--- scripts/_convert_h264_to_mp4.sh
+++ scripts/_convert_h264_to_mp4.sh
@@ -1,8 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
-# find files
-find ~/data/ -type f -name "*.h264"
+fps=60
 
-# check if .mp4 exists
+find ~/data/ -type f -name "*.h264" -print0 |
+    while IFS= read -r -d '' file; do
+	converted_file=${file}.mp4
 
-# MP4Box with correct fps from settings
+	if stat -t $converted_file >/dev/null 2>&1
+	then
+		:	# converted_file exists
+	else
+		printf "\n ==>> Converting file: $f"
+		MP4Box -fps $fps -add $file $converted_file
+	fi
+    done
--- scripts/upload_data.sh
+++ scripts/upload_data.sh
@@ -4,8 +4,11 @@
 find ~/data/ -type f -name "*.pyc" -delete
 find ~/data/ -type d -name "__pycache__" -exec rm -rfv {} +
 
-# exclude test_subject from upload
-# exclude unnamed acquisitions from video source
-# exclude test protocols like _test_flush_water
-# exclude old "main_project" folder
-rsync -av $1 --info=progress2 ~/data/* larsr@ssh.swc.ucl.ac.uk:/nfs/winstor/sjones/users/lars/ACQUISITION/behaviour/ --exclude="_test_subject" --exclude="default_acq_*" --exclude="*___test__*" --exclude="*.csv.bak.2*"
+rsync -av $1 --info=progress2 ~/data/* larsr@ssh.swc.ucl.ac.uk:/nfs/winstor/sjones/users/lars/ACQUISITION/behaviour/ \
+	--exclude="_test_subject" \
+	--exclude="default_acq_*" \
+	--exclude="*___test__*" \
+	--exclude="*.csv.bak.2*" \
+	--exclude="_*"
+
+printf "\nDONE\n"
