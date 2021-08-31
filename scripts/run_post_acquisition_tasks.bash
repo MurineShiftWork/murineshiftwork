2a26f9208ecc9b01a89965628f1885604208a6bf
Lars B. Rollik <L.B.Rollik@pm.me>
2021-08-31 08:35:43 +0100

    script fixes

--- scripts/run_post_acquisition_tasks.bash
+++ scripts/run_post_acquisition_tasks.bash
@@ -3,8 +3,8 @@
 eval script_path="~/murine_shift_work/scripts"
 
 bash  $script_path/_move_data_to_central.bash
-bash  $script_path/_convert_h264_to_mp4.sh
-bash  $script_path/_clean_msw_files.sh
+bash  $script_path/_convert_h264_to_mp4.bash
+bash  $script_path/_clean_msw_files.bash
 
 bash  $script_path/upload_data.sh
 # printf "\n\n -->> Manually UPLOAD acquisition data with $script_path/upload_data.sh \n\n" 
