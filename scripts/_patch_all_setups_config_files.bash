05b0a6efa7082387ec53d80c2dcd589ae58154d8
Lars B. Rollik <L.B.Rollik@pm.me>
2021-08-31 16:53:55 +0100

    fix patching config files

--- scripts/_patch_all_setups_config_files.bash
+++ scripts/_patch_all_setups_config_files.bash
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+source_dir="/mnt/maindata/CONFIG_FILES"
+
 for setup in setup1 setup2 setup3 setup4 setup5
 do
-	echo $setup
-	rsync -av ~/murine_shift_work/tests/CONFIG_FILES $setup:~/
+	echo =================  $setup
+	rsync -av $source_dir $setup:'~/'
 done
 
 for setup in setup2 setup3 setup4 setup5
 do
-	echo $setup
+	echo =================  $setup
 	rsync -av ~/.ssh/config $setup:~/.ssh/
 done
