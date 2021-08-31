fbba837549b1fd8a838a497cf837c859db14afb0
Lars B. Rollik <L.B.Rollik@protonmail.com>
2021-08-31 20:32:54 +0100

    fix configobj in dependencies

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     install_requires=[
+        "configobj",
         "opencv-python==3.4.5.20",
         "PyQtWebEngine",
         "pyqtgraph",
