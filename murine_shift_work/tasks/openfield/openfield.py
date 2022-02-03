b912fb67a2d6791cddc6e6273080dde8a4c6d1d2
Lars B. Rollik <L.B.Rollik@protonmail.com>
2022-02-03 17:11:09 +0000

    added TTL for new propagation structure

--- murine_shift_work/tasks/openfield/openfield.py
+++ murine_shift_work/tasks/openfield/openfield.py
@@ -1,9 +1,22 @@
 from murine_shift_work.tasks.periodic_trigger_with_video.periodic_trigger_with_video import (
-    run_task,
+    run_task as run_task_core,
 )
 from murine_shift_work.tasks.periodic_trigger_with_video.periodic_trigger_with_video import (
     Task,
 )
+
+
+def run_task(**args_dict):
+
+    args_dict.update(
+        {
+            "ttl_identifier_sequence": "sLssss",
+            "trigger_iti": 1,
+            "n_max_trials": 10,
+        }
+    )
+    run_task_core(**args_dict)
+
 
 if __name__ == "__main__":
     print("main")
