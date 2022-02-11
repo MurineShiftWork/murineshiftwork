971a9d9040d438afa94f356de8119b358c107580
Lars B. Rollik <L.B.Rollik@protonmail.com>
2022-02-11 10:25:05 +0000

    saving data for test protocol

--- murine_shift_work/tasks/_test_ttl_outputs/_test_ttl_outputs.py
+++ murine_shift_work/tasks/_test_ttl_outputs/_test_ttl_outputs.py
@@ -1,6 +1,9 @@
 import logging
 import time
+from pathlib import Path
 
+import numpy as np
+import pandas as pd
 from pybpodapi.bpod import Bpod
 
 from murine_shift_work.logic.specific_state_machines import (
@@ -10,11 +13,49 @@ from murine_shift_work.logic.task_process import TaskProcess
 from murine_shift_work.logic.task_process import TaskRunner
 
 
+class TaskData:
+    save_path = None
+    data = []
+
+    def __init__(self, save_path=None):
+        super(TaskData, self).__init__()
+        self.save_path = save_path
+
+    def append(self, trial_index=None, trial_data=None, **info_dict_extension):
+        # If is TTL trial
+        first_state_name = str(list(trial_data["States timestamps"].keys())[0]).lower()
+        if trial_index < 1 and first_state_name.startswith("pulse"):
+            trial_type = "ttl"
+        else:
+            trial_type = "task"
+            # trial_data["info"] = {"trial_type": "ttl", "trial_index": trial_index}
+
+        trial_data["info"] = {
+            "trial_type": trial_type,
+            "trial_index": trial_index,
+            **info_dict_extension,
+        }
+
+        self.data.append(trial_data)
+
+    def save(self, save_path=None):
+        save_path = save_path or self.save_path
+        logging.debug("Saving task control data..")
+        dt = time.time()
+        df = pd.DataFrame(self.data)
+        df.to_pickle(str(save_path) + ".df.pkl")
+        logging.debug(f"Saved data in {np.round(time.time() - dt, 2)}s.")
+
+
 class Task(TaskRunner):
     def run(self) -> None:
+        bnc_channels_to_test = [1, 2]
         test_sequence = "LLssLLss"
 
-        for bnc_channel in [1, 2]:
+        save_path = Path(self.bpod.workspace_path) / self.bpod.session_name
+        task_data = TaskData(save_path=save_path)
+
+        for trial_index, bnc_channel in enumerate(bnc_channels_to_test):
             logging.info(
                 f"Testing BNC outputs with TTL sequence {test_sequence} on BNC channel {bnc_channel}"
             )
@@ -28,6 +69,12 @@ class Task(TaskRunner):
 
             if not self.bpod.run_state_machine(sma):
                 print("nothing returned")
+
+            task_data.append(
+                trial_index=trial_index,
+                trial_data=self.bpod.session.current_trial.export(),
+            )
+            task_data.save()
 
 
 def run_task(**kwargs):
