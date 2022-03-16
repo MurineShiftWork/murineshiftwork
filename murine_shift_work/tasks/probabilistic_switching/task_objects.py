8ca9d98e16ea26284df972f7c09c44609ea01dc8
Lars B. Rollik <L.B.Rollik@protonmail.com>
2022-03-16 09:52:56 +0000

    probability fix

--- murine_shift_work/tasks/probabilistic_switching/task_objects.py
+++ murine_shift_work/tasks/probabilistic_switching/task_objects.py
@@ -162,6 +162,8 @@ class TaskControl(object):
                 f"Given probability proportions {new_prob}. Now converting to percentages (x/100)"
             )
             self.probability_left, self.probability_right = [x / 100 for x in new_prob]
+        else:
+            self.probability_left, self.probability_right = new_prob
 
         logging.info(
             f"New block #{self.block_number} after trial #{self.trial_index} with probabilities {self.probability_left}/{self.probability_right}"
