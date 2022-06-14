bca2f5944a1cbd18ece8de4a69d24dc8e68b0a12
Lars Bjoern Rollik <l.b.rollik@pm.me>
2022-06-14 09:46:21 +0100

    new paradigm for head fixed / spout task

---
+++ murine_shift_work/tasks/two_armed_bandit_with_spouts/two_armed_bandit_with_spouts.py
@@ -0,0 +1,12 @@
+# Two-armed bandit task for head-fixed setup with retractable spouts.
+import numpy as np
+
+
+class TaskData:
+    def __init__(self):
+        pass
+
+
+class Task:
+    def __init__(self):
+        pass
