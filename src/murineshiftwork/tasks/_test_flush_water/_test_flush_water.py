2f62ece68d9dd756b3834c49df421d90e033939e
Lars B. Rollik <L.B.Rollik@protonmail.com>
2026-05-15 16:45:08 +0100

    flush protocol extension for sequential flush

--- src/murineshiftwork/tasks/_test_flush_water/_test_flush_water.py
+++ src/murineshiftwork/tasks/_test_flush_water/_test_flush_water.py
@@ -14,13 +14,17 @@ class Task(TaskRunner):
 
         valve_opening_time_ms = float(s.get("VALVE_OPENING_TIME_MS", 50.0))
         valve_numbers = list(s.get("VALVE_NUMBERS", [1, 3]))
-        n_flush_cycles = int(s.get("N_FLUSH_CYCLES", 1))
+        n_flush_cycles = int(s.get("N_FLUSH_CYCLES", 3))
         inter_flush_interval_s = float(s.get("INTER_FLUSH_INTERVAL_S", 1.0))
         flush_sequentially = bool(s.get("FLUSH_VALVES_SEQUENTIALLY", False))
 
+        # When sequential, divide cycle time equally across valves
+        per_valve_time_ms = valve_opening_time_ms / len(valve_numbers) if flush_sequentially else valve_opening_time_ms
+
         logging.info(
-            f"Flush: valves={valve_numbers}, open_time={valve_opening_time_ms}ms, "
-            f"cycles={n_flush_cycles}, interval={inter_flush_interval_s}s, "
+            f"Flush: valves={valve_numbers}, cycle_time={valve_opening_time_ms}ms, "
+            + (f"per_valve={per_valve_time_ms:.1f}ms, " if flush_sequentially else "")
+            + f"cycles={n_flush_cycles}, interval={inter_flush_interval_s}s, "
             f"sequential={flush_sequentially}"
         )
 
@@ -32,10 +36,8 @@ class Task(TaskRunner):
                 for valve in valve_numbers:
                     if not self.continue_task:
                         break
-                    self._flush_valves([valve], valve_opening_time_ms)
-                    logging.info(f"Flush cycle {cycle + 1}/{n_flush_cycles} valve {valve} done")
-                    if valve != valve_numbers[-1]:
-                        time.sleep(inter_flush_interval_s)
+                    self._flush_valves([valve], per_valve_time_ms)
+                logging.info(f"Flush cycle {cycle + 1}/{n_flush_cycles} done (sequential: {valve_numbers})")
             else:
                 self._flush_valves(valve_numbers, valve_opening_time_ms)
                 logging.info(f"Flush cycle {cycle + 1}/{n_flush_cycles} done")
